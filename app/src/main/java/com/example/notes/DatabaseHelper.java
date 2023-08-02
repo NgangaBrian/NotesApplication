8c09969c8db1129f26cd95fa46b6c1744439a5c4
Brian <ngangabrian724@gmail.com>
2023-08-02 22:59:06 +0300

    Second Commit

--- app/src/main/java/com/example/notes/DatabaseHelper.java
+++ app/src/main/java/com/example/notes/DatabaseHelper.java
@@ -5,8 +5,12 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -14,18 +18,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String databaseName = "Notes.db";
 
     public DatabaseHelper(@Nullable Context context) {
-        super(context, "Notes.db", null, 1);
+
+        super(context, "Notes.db", null, 2);
     }
 
     @Override
     public void onCreate(SQLiteDatabase myDatabase) {
         myDatabase.execSQL("create Table allusers(email TEXT primary key, password TEXT)");
-        myDatabase.execSQL("Create Table notes(id INT primary key AUTOINCREMENT, email TEXT, Title TEXT, NoteDetails TEXT, FOREIGN KEY (email) REFERENCES allusers(email))");
-    }
+        String query = "Create Table notes(id INT PRIMARY KEY AUTOINCREMENT, " +
+                        "Title TEXT, " +
+                        "Description TEXT, " +
+                        "DateCreated TEXT, " +
+                        "TimeCreated)";
+        
+              myDatabase.execSQL(query);    }
 
     @Override
     public void onUpgrade(SQLiteDatabase myDatabase, int i, int i1) {
         myDatabase.execSQL("drop Table if exists allusers");
+        myDatabase.execSQL("Drop Table if exists notes");
+        onCreate(myDatabase);
     }
     public Boolean insertData(String email, String password){
         SQLiteDatabase myDatabase = this.getWritableDatabase();
@@ -62,5 +74,39 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         else {
             return false;
         }
+    }
+    public long AddNote (Note noteModel){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Title", noteModel.getTitle());
+        contentValues.put("Description", noteModel.getDescription());
+        contentValues.put("DateCreated", noteModel.getNoteDate());
+        contentValues.put("TimeCreated", noteModel.getNoteTime());
+
+        long ID = db.insert("notes", null, contentValues);
+        Log.d("Inserted", "id -->" + ID);
+        return ID;
+    }
+    public List<Note> getNote(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        List<Note> allNote = new ArrayList<>();
+
+        String queryStatement = "Select * From notes";
+        Cursor cursor = db.rawQuery(queryStatement, null);
+
+        if(cursor.moveToFirst()){
+            do {
+                Note noteModel = new Note();
+                noteModel.setId(cursor.getInt(0));
+                noteModel.setTitle(cursor.getString(1));
+                noteModel.setDescription(cursor.getString(2));
+                noteModel.setNoteDate(cursor.getString(3));
+                noteModel.setNoteTime(cursor.getString(4));
+
+                allNote.add(noteModel);
+
+            } while (cursor.moveToNext());
+        }
+        return allNote;
     }
 }
--- app/src/main/java/com/example/notes/add_note.java
+++ app/src/main/java/com/example/notes/add_note.java
@@ -1,29 +1,76 @@
 package com.example.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class add_note extends AppCompatActivity {
+
+    public EditText title, description;
+    public Button add;
+    String todayDate, currentTime;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
 
-        Button add = findViewById(R.id.add);
 
+
+        title = findViewById(R.id.titleinput);
+        description = findViewById(R.id.description);
+
+        calendar = Calendar.getInstance();
+        todayDate = calendar.get(Calendar.YEAR) + "/" +calendar.get(Calendar.MONTH) + "/" + calendar.get(Calendar.DAY_OF_MONTH);
+        currentTime = pad(calendar.get(Calendar.HOUR))+":"+ pad(calendar.get(Calendar.MINUTE));
+
+        Log.d("Calender", "Date and Time" + todayDate+"and"+currentTime);
+
+        add = findViewById(R.id.add);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 blinkButton(view);
+                Note noteModel = new Note(title.getText().toString(), description.getText().toString(), todayDate, currentTime);
+                DatabaseHelper db = new DatabaseHelper(add_note.this);
+                db.AddNote(noteModel);
+
+                Intent intent = new Intent(add_note.this, MainActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(add_note.this, "Note Saved", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+    private String pad(int s) {
+        if(s < 0)
+            return  "0"+s;
+        return String.valueOf(s);
+    }
+
     private void blinkButton(View view){
         view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.blink_animation));
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() ==  android.R.id.home);
+        finish();
+        return true;
+
+    }
+
 }
